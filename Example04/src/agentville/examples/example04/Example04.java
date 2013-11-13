be61687544fd792f9f47d3cd48b8eba71fa10ab6
Marco <marco.steffens@gmail.com>
2013-11-13 19:21:32 +0100

    init Example04

---
+++ Example04/src/agentville/examples/example04/Example04.java
@@ -0,0 +1,12 @@
+package agentville.examples.example04;
+
+import jade.core.Agent;
+
+public class Example04 extends Agent {
+
+    protected void setup() {
+    	
+        System.out.println("Hello Eclipse! This is "
+                + getLocalName());
+    }
+}
