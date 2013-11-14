07418a0aab2ed646d4f5af92ce3ce680db2062af
Marco <marco.steffens@gmail.com>
2013-11-14 07:59:56 +0100

    package-Anpassungen

---
+++ agentville/examples/example02/Example02.java
@@ -0,0 +1,12 @@
+package agentville.examples.example02;
+
+import jade.core.Agent;
+
+public class Example02 extends Agent {
+
+    protected void setup() {
+
+	System.out.println("Hello World! This is "
+				+ getLocalName());
+    }
+}
---
+++ agentville/examples/example04/Example04.java
@@ -0,0 +1,51 @@
+package agentville.examples.example04;
+
+import jade.core.Agent;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+
+public class Example04 extends Agent {
+
+    protected void setup() {
+
+		System.out.println("Hello World! This is "
+					+ getLocalName());
+    }
+ 
+    public static void main(String[] args){
+ 
+        String host;
+        int port;
+        String platform = null;     //default name
+        boolean main = true;
+ 
+        host = "localhost";
+        port = -1;          //default-port 1099
+         
+        Runtime runtime = Runtime.instance();
+        Profile profile = null;
+        AgentContainer container = null;
+ 
+        profile = new ProfileImpl(host, port, platform, main);
+         
+        //Container erzeugen
+        container = runtime.createMainContainer(profile);
+         
+         // Agenten erzeugen und startet - oder aussteigen.
+        try {
+ 
+            AgentController agent = container.createNewAgent(
+                    "Agent4",
+                    agentville.examples.example04.Example04.class.getName(),
+                    args);
+            agent.start();
+ 
+        } catch(StaleProxyException e) {
+            throw new RuntimeException(e);
+        }          
+  }
+}
