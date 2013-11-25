a0673f43d372862a1cd9abb6ad3137386831c32c
Marco <marco.steffens@gmail.com>
2013-11-25 13:50:17 +0100

    init Example05

---
+++ agentville-example05/src/main/java/agentville/examples/example05/Example05.java
@@ -0,0 +1,14 @@
+package agentville.examples.example05;
+
+import jade.core.Agent;
+
+public class Example05 extends Agent {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void setup() {
+    	
+        System.out.println("Hello Eclipse! This is "
+                + getLocalName());
+    }
+}
